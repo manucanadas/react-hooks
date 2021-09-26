f238acee36b4e6012c6040dfb03b39052ddc2cde
Manuel Canadas <nanocanadas@gmail.com>
2021-09-26 16:56:18 +1300

    Done

--- src/App.js
+++ src/App.js
@@ -1,107 +1,142 @@
-import React, {useEffect, useState} from "react";
+import * as React from "react";
 import './App.css';
-import axios from "axios";
 
-type Location = any;
 
-const fetchData = () => {
-  return axios.get("https://randomuser.me/api/?results=20")
-  .then((res) => {
-    const {results} = res.data
-    return results;
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-}
+const getUserAsync = () => {
+  return fetch("https://randomuser.me/api/?results=20",{
+    method: "GET"
+  }).then((res) => res.json())
+};
 
-const flattenLocations = (locations: Location[]) => {
-  const data = []
-  for (const {street, coordinates, timezone, ...rest} of locations)
-  data.push({
-    ...rest,
-    number: street.number,
-    name: street.name,
-    latitude: coordinates.latitude,
-    longitude: coordinates.longitude,
-  })
+const useGetUsers = () => {
+  const [response, setResponse] = React.useState();
+  React.useEffect(() => {
+    let isLoaded = true;
+    getUserAsync().then((res) => {
+      if (isLoaded) {
+        setResponse(res.results)
+      }
+    });
+    return () => {
+      isLoaded = false;
+    };
+  }, []);
+  return response;
+};
 
-  const flattenedLocationHeaders = extractObjectKeys(data[0]);
-  
-  console.log(flattenedLocationHeaders)
-  return {headers: flattenedLocationHeaders, data}
-  
-}
+const getFullName = (name) => {
+  return `${name.title} ${name.first} ${name.last}`; 
+};
 
-const extractObjectKeys = (object: any) => {
-  let objectKeys: string[] = [];
+const matchesKey = (item, filterKey) => {
+  const attributes = [
+    item.location.city,
+    item.location.state,
+    item.location.country,
+    item.location.postcode,
+    item.location.street.number,
+    getFullName(item.name),
+    item.location.coordinates.latitude,
+    item.location.coordinates.longitude,
+  ];
+  return attributes.some((attr) =>
+  String(attr).toLowerCase().includes(filterKey.toLowerCase())
+  );
+};
 
-  Object.keys(object).forEach(objectKey => {
-    const value = object[objectKey];
-    if (typeof value !== 'object') {
-      objectKeys.push(objectKey);
-    }else {
-      objectKeys = [...objectKeys, ...extractObjectKeys(value)];
-    }
-  })
-  return objectKeys 
-}
+const sortByField = (a, b, sortField, sortDirection) => {
+  let itemA, itemB;
+  if(sortField === "city") {
+    itemA = a.location.city;
+    itemB = b.location.city;
+  }else if(sortField === "state") {
+    itemA = a.location.state;
+    itemB = b.location.state;
+  }else if(sortField === "country") {
+    itemA = a.location.country;
+    itemB = b.location.country;
+  }else if(sortField === "postcode") {
+    itemA = a.location.postcode;
+    itemB = b.location.postcode;
+  }else if(sortField === "number") {
+    itemA = a.location.number;
+    itemB = b.location.number;
+  }else if(sortField === "name") {
+    itemA = a.name.first + " " + a.name.last;
+    itemB = b.name.first + " " + b.name.last;
+  }else if(sortField === "latitude") {
+    itemA = a.location.coordinates.latitude;
+    itemB = b.location.coordinates.latitude;
+  }else if(sortField === "longitude") {
+    itemA = a.location.coordinates.longitude;
+    itemB = b.location.coordinates.longitude;
+  }else {
+    return 1;
+  }
+  if (sortDirection ==="ASC") {
+    return itemA < itemB ? -1 : 1;
+  }else {
+    return itemA > itemB ? 1 : -1;
+  }
+};
 
 function App() {
+  
+  const results = useGetUsers();
+  const [filterKey, setFilterKey] = React.useState("");
+  const [sortField, setSortField] = React.useState();
+  const [sortDirection, setSortDirection] = React.useState();
 
-  const [flattenedLocations, setFlattenedLocations] = useState({headers: [], data: []});
-
-  const sortColumn = (sortKey) => {
-    console.log(sortKey);
-    const newFlattenedLocations = {
-      ...flattenedLocations,
-      data: [...flattenedLocations.data]
-    };
-    newFlattenedLocations.data.sort((a, b) => {
-      const relevantValueA = a[sortKey];
-      const relevantValueB = b[sortKey];
-
-      if (relevantValueA < relevantValueB) return -1;
-      if (relevantValueA > relevantValueB) return 1;
-      return 0;
-    });
-
-    setFlattenedLocations(newFlattenedLocations)
+  const updateSort = (fieldName) => {
+    if (fieldName !== sortField) {
+      setSortField(fieldName); 
+      setSortDirection("ASC") 
+      }else {
+        if (sortDirection === "ASC") {
+          setSortDirection("DES");
+        }else {
+          setSortField();
+      }
+    }
   };
-
-  useEffect(() => {
-    fetchData().then((apiLocations) => {
-      setFlattenedLocations(flattenLocations(apiLocations.map(({ location }) => location)));
-    });
-  }, []);
 
   return (
     <div className="App">
       <h1>Hola Manu!!</h1>
       <h2>Start editing to see the magic happen</h2>
+      <input value={filterKey} onChange={(e) => setFilterKey(e.target.value)}/>
       <table>
         <thead>
           <tr>
-          {flattenedLocations.headers.map((locationString: string, locationId) => (
-        <th key={locationId}
-            onClick={() => {
-                sortColumn(locationString)
-            }}>
-              {locationString}
-              </th>
-      ))}
+            <th onClick={() => updateSort("city")}>City</th>
+            <th onClick={() => updateSort("state")}>State</th>
+            <th onClick={() => updateSort("country")}>Country</th>
+            <th onClick={() => updateSort("postcode")}>Postcode</th>
+            <th onClick={() => updateSort("number")}>Number</th>
+            <th onClick={() => updateSort("name")}>Name</th>
+            <th onClick={() => updateSort("latitude")}>Latitude</th>
+            <th onClick={() => updateSort("longitude")}>Longitude</th>
           </tr>
         </thead>
         <tbody>
-        {flattenedLocations.data.map((location: any, locationId) => (
-        <tr key={locationId}>
-          {flattenedLocations.headers.map((header, headerId) => (
-          <td key={headerId}>{location[header]}</td>))}
-          </tr>
-      ))}
+          {results && 
+          results
+          .filter((item) => matchesKey(item, filterKey))
+          .sort((a, b) => sortByField(a, b, sortField, sortDirection))
+          .map((item) => (
+            <tr key={item.login.uuid}>
+              <td>{item.location.city}</td>
+              <td>{item.location.state}</td>
+              <td>{item.location.country}</td>
+              <td>{item.location.postcode}</td>
+              <td>{item.location.street.number}</td>
+              <td>{getFullName(item.name)}</td>
+              <td>{item.location.coordinates.latitude}</td>
+              <td>{item.location.coordinates.longitude}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      
     </div>
   );
 }
